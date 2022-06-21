1547f3eb84ee854bab4f45f4d8b948901fed7e72
Daniel Maldonado <damaldy19@hotmail.com>
2022-06-20 21:19:16 -0500

    script: querySelectors for all the elements and eventlistener for edit

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('dwqed')
+/*2.We capture the whole sticky note (the main div) in a const. */
+const notesElement = document.querySelector('.notes');
+
+/*1.Btns captured in their respective const.*/
+const editBtn = document.querySelector('.edit');
+const deleteBtn = document.querySelector('.delete');
+
+/*3.We capture the main & text areas in its variables.*/
+const main = notesElement.querySelector('.main');
+const textArea = notesElement.querySelector('textarea');
+/*I suppose that querySelector is used for ease the calling of the elements from .html file, in special the contained elements, like those (their parent is
+    .notes, represented in notesElement const).*/
+
+    
+editBtn.addEventListener('click', () => {
+    main.classList.toggle('hidden');
+    /*When edit btn is clicked, then div changes from main class to hidden class. And as we declared, .notes . hidden display none! So this means that when we click in edit btn, the area disappears. So, classList.toggle('hidden) switches the class name and apply the display we setted.*/
+    textArea.classList.toggle('hidden');
+})
